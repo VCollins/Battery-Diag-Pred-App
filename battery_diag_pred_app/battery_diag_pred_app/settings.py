b61d586773087f7b1adbfd3d963e4bb15e05572d
Vincent Collins <vincent.ghostwolf@gmail.com>
2021-10-06 17:44:37 +0000

    database configuration altered for MySQL use

--- battery_diag_pred_app/battery_diag_pred_app/settings.py
+++ battery_diag_pred_app/battery_diag_pred_app/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'battery_diag_pred_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/ubuntu/django-projects/Battery-Diag-Pred-App/battery_diag_pred_app/mysql-conn.cnf'
+        }
     }
 }
 
