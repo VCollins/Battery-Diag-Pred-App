cc257e9a3f5baf41bc9e81b538060df2d2815d13
Vincent Collins <vincent.ghostwolf@gmail.com>
2021-10-14 15:10:24 +0000

    Basic model for application applied to database

--- battery_diag_pred_app/battery_sentry/models.py
+++ battery_diag_pred_app/battery_sentry/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    userId = models.CharField(max_length=200)
+    password = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.userId
+
+class Battery(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    serialNumber = models.CharField(max_length=200)
+    originalOutputVoltage = models.FloatField()
+    dateOfPurchase = models.DateField()
+    capacity = models.CharField(max_length=10)
+    batteryType = models.CharField(max_length=200)
+    batteryDimensions = models.CharField(max_length=200)
+    weight = models.FloatField()
+    terminalLayout = models.CharField(max_length=200)
+    warranty = models.CharField(max_length=10)
+    users = models.ManyToManyField(User, through='UserBatteryDetail')
+
+    def __str__(self):
+        return self.manufacturer + ' ' + self.model
+
+class DepletionReading(models.Model):
+    batteryId = models.ForeignKey(Battery, on_delete=models.CASCADE)
+    depletionVoltageReading = models.FloatField()
+    readingDateTime = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.depletionVoltageReading + ' : ' + self.readingDateTime
+
+class ChargeReading(models.Model):
+    batteryId = models.ForeignKey(Battery, on_delete=models.CASCADE)
+    chargeVoltageReading = models.FloatField()
+    readingDateTime = models.DateTimeField()
+
+    def __str__(self):
+        return self.chargeVoltageReading + ' : ' + self.readingDateTime
+
+class UserBatteryDetail(models.Model):
+    battery = models.ForeignKey(Battery, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
